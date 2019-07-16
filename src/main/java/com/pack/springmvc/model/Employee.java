0d24bcde472dc149e7f3a7ac45d663c14bc33620
kesavarthini <kesi@publicisgroupe.net>
2019-07-16 17:58:58 +0530

    display findbyid

--- src/main/java/com/pack/springmvc/model/Employee.java
+++ src/main/java/com/pack/springmvc/model/Employee.java
@@ -10,6 +10,37 @@ import lombok.ToString;
 public class Employee {
 	private int id;
 	private String name;
+	public Employee(int id, String name, int age, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 	private int age;
 	private double salary;
 }
